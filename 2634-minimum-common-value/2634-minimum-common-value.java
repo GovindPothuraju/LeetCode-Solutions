233231a49527d17452035d0b24fd7eaf590699d9
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-03-31 21:26:02 +0530

    Time: 2 ms (84.07%), Space: 62.8 MB (35.25%) - LeetHub

--- 2634-minimum-common-value/2634-minimum-common-value.java
+++ 2634-minimum-common-value/2634-minimum-common-value.java
@@ -1,7 +1,5 @@
 class Solution {
     public int getCommon(int[] nums1, int[] nums2) {
-        Arrays.sort(nums1);
-        Arrays.sort(nums2);
         int i=0,j=0;
         int res=-1;
         while(i<nums1.length && j<nums2.length){
