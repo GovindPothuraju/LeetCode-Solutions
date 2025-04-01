bdfc16e48ba7b819d9e721c6d25d9fe919640bfe
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-04-01 22:17:18 +0530

    Time: 0 ms (100%), Space: 40.4 MB (81.44%) - LeetHub

---
+++ 1806-count-of-matches-in-tournament/1806-count-of-matches-in-tournament.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numberOfMatches(int n) {
+        int res=0;
+        while(n>1){
+            res+=n/2;
+            if(n%2 != 0){
+                res+=1;
+            }
+            n=n/2;
+        }
+        return res;
+    }
+}
