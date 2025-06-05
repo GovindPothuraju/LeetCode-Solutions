a0b92ef8565d2bbe73b293c5b0bb5705a5264388
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-06-05 09:43:40 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProduct(int[] nums) {
         int result=Integer.MIN_VALUE;
         int n=nums.length;
-        for(int i=0;i<n;i++){
+        /*for(int i=0;i<n;i++){
             int mul=1;
             for(int j=i;j<n;j++){
                 mul*=nums[j];
@@ -10,6 +10,29 @@ class Solution {
             }
 
         }
-        return result;
+        return result;*/
+        int maxp=Integer.MIN_VALUE;
+        int prefix=1;
+        int suffix=1;
+        for(int i=0;i<n;i++){
+            prefix*=nums[i];
+            if(prefix==0){
+                prefix=1;
+            }
+            maxp=Math.max(maxp,prefix);
+        }
+        for(int i=n-1;i>=0;i--){
+            suffix*=nums[i];
+            if(suffix==0){
+                suffix=1;
+            }
+            maxp=Math.max(maxp,suffix);
+        }
+        if(maxp==1){
+            return 0;
+        }else{
+            return maxp;
+        }
+
     }
 }
