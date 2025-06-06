fd8abfbf8c4053576f19e6ec835b8b53b7c047d8
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-06-06 11:31:48 +0530

    Time: 30 ms (69.58%), Space: 51.4 MB (83.49%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -19,7 +19,7 @@ class Solution {
         return oarr;*/
 
         // using hashing
-        List<List<Integer>>  oarr =new ArrayList<>();
+       /*List<List<Integer>>  oarr =new ArrayList<>();
         HashSet<List<Integer>> set=new HashSet<>();
         int n=nums.length;
         for(int i=0;i<n;i++){
@@ -39,6 +39,32 @@ class Solution {
         }
         
         oarr.addAll(set);
+        return oarr;*/
+
+        // using two pointers
+        List<List<Integer>>  oarr =new ArrayList<>();
+        int n=nums.length;
+        Arrays.sort(nums);
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]) continue;
+
+            int j=i+1,k=n-1;
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
+                if(sum>0){
+                    k--;
+                }else if(sum<0){
+                    j++;
+                }else{
+                    oarr.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                    j++;
+                    k--;
+                    while(j<k && nums[j]==nums[j-1]) j++;
+                    while(j<k && nums[k]==nums[k+1]) k--;
+                }
+            }
+        }
         return oarr;
     }
 }
