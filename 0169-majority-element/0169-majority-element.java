650d9efa4a73cfedee1c4f0d87e18f01ccb86f7a
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-06-06 10:02:05 +0530

    Time: 1 ms (99.84%), Space: 52.9 MB (65.67%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,8 +1,22 @@
 class Solution {
     public int majorityElement(int[] nums) {
         // solution
-        Arrays.sort(nums);
+       /* Arrays.sort(nums);
         int n=nums.length/2;
-        return nums[n];
+        return nums[n];*/
+        int freq=0;
+        int ans=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(freq==0){
+                ans=nums[i];
+            }
+            if(ans==nums[i]){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
+        return ans;
     }
 }
