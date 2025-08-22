26763696b2e804057cdbc420d9dd2235de3fd99c
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-08-22 11:20:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,11 @@
+class Solution {
+    public long subArrayRanges(int[] arr) {
+        int Total=0;
+        int smallest=0;
+        int largest=0;
+        int n=arr.length;
+        int nse[]=new int[n];
+        int nse[]=new int[n];
+        int nse[]=new int[n];
+    }
+}
