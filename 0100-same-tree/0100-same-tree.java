bd1c712c53bece329152d5ee3e668ae86928a70e
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-10-21 16:47:20 +0530

    Time: 0 ms (100%), Space: 40.9 MB (85.26%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,17 +14,48 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p==null && q==null){
-            return true;
-        }
-        if(p==null || q==null){
-            return false;
-        }
-        if(p.val != q.val){
-            return false;
-        }
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
-    }
+    // using recursion
+    // public boolean isSameTree(TreeNode p, TreeNode q) {
+    //     if(p==null && q==null){
+    //         return true;
+    //     }
+    //     if(p==null || q==null){
+    //         return false;
+    //     }
+    //     if(p.val != q.val){
+    //         return false;
+    //     }
+    //     return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+    // }
    
+
+
+   // using iteration by using BFS
+   public boolean isSameTree(TreeNode p, TreeNode q) {
+        if(p==null && q==null) return true;
+        if(p==null || q==null) return false;
+
+        Queue<TreeNode> q1=new LinkedList<>();
+        Queue<TreeNode> q2=new LinkedList<>();
+
+        q1.offer(p);
+        q2.offer(q);
+
+        while(!q1.isEmpty() && !q2.isEmpty()){
+            TreeNode curr1=q1.poll();
+            TreeNode curr2=q2.poll();
+
+            if(curr1==null && curr2==null) continue;
+            if(curr1==null || curr2==null) return false;
+
+            if(curr1.val != curr2.val){
+                return false;
+            }
+            q1.offer(curr1.left);
+            q2.offer(curr2.left);
+            q1.offer(curr1.right);
+            q2.offer(curr2.right);
+        }
+        return true;
+   }
 }
