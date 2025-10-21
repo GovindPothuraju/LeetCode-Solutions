b7f530b2fcbebde7e11d4cfcdf59acd0d6fb566c
GovindPothuraju <govindpothuraju2006@gmail.com>
2025-10-21 13:18:55 +0530

    Time: 1 ms (12.37%), Space: 42.4 MB (6.94%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -14,19 +14,41 @@
  * }
  */
 class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if(root==null) return true;
-        return isMirror(root.left, root.right);
-    }
-    private boolean isMirror(TreeNode curr1,TreeNode curr2){
-        // ✅ Both null → symmetric
-        if (curr1 == null && curr2 == null) return true;
+    // using Recursion
+    // public boolean isSymmetric(TreeNode root) {
+    //     if(root==null) return true;
+    //     return isMirror(root.left, root.right);
+    // }
+    // private boolean isMirror(TreeNode curr1,TreeNode curr2){
+    //     // ✅ Both null → symmetric
+    //     if (curr1 == null && curr2 == null) return true;
 
-        // ✅ Only one null → not symmetric
-        if (curr1 == null || curr2 == null) return false;
+    //     // ✅ Only one null → not symmetric
+    //     if (curr1 == null || curr2 == null) return false;
  
-        return (curr1.val == curr2.val)  && 
-                isMirror(curr1.left, curr2.right) && 
-                isMirror(curr1.right, curr2.left);
+    //     return (curr1.val == curr2.val)  && 
+    //             isMirror(curr1.left, curr2.right) && 
+    //             isMirror(curr1.right, curr2.left);
+    // }
+
+
+    // using iteration by using BFS and queue
+    public boolean isSymmetric(TreeNode root) {
+        Queue<TreeNode> q= new LinkedList<>();
+        q.offer(root.left);
+        q.offer(root.right);
+        while(!q.isEmpty()){
+            TreeNode p1=q.poll();
+            TreeNode p2=q.poll();
+
+            if(p1==null && p2==null)  continue;
+            if(p1==null || p2==null || p1.val != p2.val) return false;
+
+            q.offer(p1.left);
+            q.offer(p2.right);
+            q.offer(p1.right);
+            q.offer(p2.left);
+        }
+        return true;
     }
 }
