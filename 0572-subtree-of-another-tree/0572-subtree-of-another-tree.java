9225a8bade847a8b18011cc274ab7e61acddd65e
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-09 16:54:17 +0530

    Time: 5 ms (14.1%), Space: 46.5 MB (32.62%) - LeetHub

--- 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
+++ 0572-subtree-of-another-tree/0572-subtree-of-another-tree.java
@@ -14,17 +14,37 @@
  * }
  */
 class Solution {
+    // public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+    //     if(subRoot==null) return true;
+    //     if(root==null) return false;
+    //     if(sameTree(root,subRoot)){
+    //         return true;
+    //     }
+    //     return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+    // }
+    // public boolean sameTree(TreeNode p,TreeNode q){
+    //     if(p==null && q==null) return true;
+    //     if(p==null || q==null) return false;
+    //     return p.val==q.val && sameTree(p.left,q.left) && sameTree(p.right,q.right);
+    // }
+    String hash="";
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(subRoot==null) return true;
-        if(root==null) return false;
-        if(sameTree(root,subRoot)){
-            return true;
-        }
-        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+        String hashroot = serialize(root);
+        String hashsubroot = serialize(subRoot);
+        return hashroot.contains(hashsubroot);
     }
-    public boolean sameTree(TreeNode p,TreeNode q){
-        if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        return p.val==q.val && sameTree(p.left,q.left) && sameTree(p.right,q.right);
+    public String serialize(TreeNode curr){
+        StringBuilder sb=new StringBuilder();
+        traversal(curr,sb);
+        return sb.toString();
+    }
+    public void traversal(TreeNode curr,StringBuilder sb){
+        if(curr==null){
+            sb.append("-#");
+            return;
+        }
+        sb.append("-").append(curr.val);
+        traversal(curr.left,sb);
+        traversal(curr.right,sb);
     }
 }
