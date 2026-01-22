e234b45adf15d3c0dbb05c99da03e88992e12f6e
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-23 00:03:43 +0530

    Time: 10 ms (99.48%), Space: 43.9 MB (84.76%) - LeetHub

--- 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
+++ 0863-all-nodes-distance-k-in-binary-tree/0863-all-nodes-distance-k-in-binary-tree.java
@@ -8,58 +8,107 @@
  * }
  */
 class Solution {
-    HashMap<TreeNode,TreeNode> parent=new HashMap<>();
+    // using DFS & BFS
+    // HashMap<TreeNode,TreeNode> parent=new HashMap<>();
+    // public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+    //     // build parent tree
+    //     buildParent(root,null);
+
+    //     //create q and visited
+    //     Queue<TreeNode> q=new LinkedList<>();
+    //     HashSet<TreeNode> visited=new HashSet<>();
+
+    //     int distance=0;
+    //     // initially insert target
+    //     q.offer(target);
+    //     visited.add(target);
+    //     while(!q.isEmpty()){
+    //         int n=q.size();
+
+    //         if(distance==k){
+    //             List<Integer> res=new ArrayList<>();
+    //             for(TreeNode t:q){
+    //                 res.add(t.val);
+    //             }
+    //             return res;
+    //         }
+
+    //         for(int i=0;i<n;i++){
+    //             TreeNode temp=q.poll();
+    //             //left
+    //             if(temp.left != null && !visited.contains(temp.left)){
+    //                 q.offer(temp.left);
+    //                 visited.add(temp.left);
+    //             }
+    //             //right
+    //             if(temp.right != null && !visited.contains(temp.right)){
+    //                 q.offer(temp.right);
+    //                 visited.add(temp.right);
+    //             }
+    //             //parent
+    //             TreeNode par=parent.get(temp);
+    //             if(par!= null && !visited.contains(par)){
+    //                 q.offer(par);
+    //                 visited.add(par);
+    //             }
+    //         }
+    //         distance++;
+    //     }
+    //     return new ArrayList<>();
+    // }
+
+    // private void buildParent(TreeNode curr,TreeNode par){
+    //     if(curr==null) return ;
+    //     parent.put(curr,par);
+    //     buildParent(curr.left,curr);
+    //     buildParent(curr.right,curr);
+    // }
+
+
+    // Using recursion 
+    List<Integer> res=new ArrayList<>();
     public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
-        // build parent tree
-        buildParent(root,null);
-
-        //create q and visited
-        Queue<TreeNode> q=new LinkedList<>();
-        HashSet<TreeNode> visited=new HashSet<>();
-
-        int distance=0;
-        // initially insert target
-        q.offer(target);
-        visited.add(target);
-        while(!q.isEmpty()){
-            int n=q.size();
-
-            if(distance==k){
-                List<Integer> res=new ArrayList<>();
-                for(TreeNode t:q){
-                    res.add(t.val);
-                }
-                return res;
-            }
-
-            for(int i=0;i<n;i++){
-                TreeNode temp=q.poll();
-                //left
-                if(temp.left != null && !visited.contains(temp.left)){
-                    q.offer(temp.left);
-                    visited.add(temp.left);
-                }
-                //right
-                if(temp.right != null && !visited.contains(temp.right)){
-                    q.offer(temp.right);
-                    visited.add(temp.right);
-                }
-                //parent
-                TreeNode par=parent.get(temp);
-                if(par!= null && !visited.contains(par)){
-                    q.offer(par);
-                    visited.add(par);
-                }
-            }
-            distance++;
-        }
-        return new ArrayList<>();
+            traversal(root,target,k);
+            return res;
     }
+    private int traversal(TreeNode curr, TreeNode target, int k){
+        if(curr==null) return -1;
 
-    private void buildParent(TreeNode curr,TreeNode par){
-        if(curr==null) return ;
-        parent.put(curr,par);
-        buildParent(curr.left,curr);
-        buildParent(curr.right,curr);
+        if(curr==target){
+            collectDown(curr,k);
+            return 0;
+        }
+        int leftDist=traversal(curr.left,target,k);
+        if(leftDist != -1){
+            int currDist=leftDist+1;
+
+            if(currDist==k){
+                res.add(curr.val);
+            }else{
+                collectDown(curr.right,k-currDist-1);
+            }
+            return currDist;
+        }
+        int rightDist = traversal(curr.right,target,k);
+        if(rightDist != -1){
+            int currDist=rightDist+1;
+
+            if(currDist==k){
+                res.add(curr.val);
+            }else{
+                collectDown(curr.left,k-currDist-1);
+            }
+            return currDist;
+        }
+        return -1;
+    }
+    private void collectDown(TreeNode curr,int dist){
+        if(curr==null || dist<0)return;
+        if(dist==0){
+            res.add(curr.val);
+            return;
+        }
+        collectDown(curr.left,dist-1);
+        collectDown(curr.right,dist-1);
     }
 }
