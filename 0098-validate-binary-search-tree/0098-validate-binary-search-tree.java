3515abd68c70d577faf2631cc0bfe05281203b88
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-29 23:59:15 +0530

    Time: 0 ms (100%), Space: 45.4 MB (30.63%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -14,20 +14,35 @@
  * }
  */
 class Solution {
-    ArrayList<Integer> res=new ArrayList<>();
+    // 1. using inorder traversal
+    // ArrayList<Integer> res=new ArrayList<>();
+    // public boolean isValidBST(TreeNode root) {
+    //     traversal(root);
+    //     for(int i=1;i<res.size();i++){
+    //         if(res.get(i-1)>=res.get(i)){
+    //             return false;
+    //         }
+    //     }
+    //     return true;
+    // }
+    // private void traversal(TreeNode curr){
+    //     if(curr==null)return;
+    //     traversal(curr.left);
+    //     res.add(curr.val);
+    //     traversal(curr.right);
+    // }
+    
     public boolean isValidBST(TreeNode root) {
-        traversal(root);
-        for(int i=1;i<res.size();i++){
-            if(res.get(i-1)>=res.get(i)){
-                return false;
-            }
-        }
-        return true;
+        boolean res=traversal(root,Long.MIN_VALUE,Long.MAX_VALUE);
+        return res;
     }
-    private void traversal(TreeNode curr){
-        if(curr==null)return;
-        traversal(curr.left);
-        res.add(curr.val);
-        traversal(curr.right);
+    private boolean traversal(TreeNode curr,long lo,long hi){
+        if(curr==null)return true;
+        if(curr.val<=lo || curr.val>=hi){
+           return false;
+        } 
+        boolean left=traversal(curr.left,lo,curr.val);
+        boolean right=traversal(curr.right,curr.val,hi);
+        return left && right;
     }
 }
