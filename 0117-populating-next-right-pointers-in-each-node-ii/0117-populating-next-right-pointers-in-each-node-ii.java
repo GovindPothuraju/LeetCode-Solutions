87ff9ea7758772ab3f42fcf77bd430b84fa6b41b
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-29 12:39:44 +0530

    Time: 2 ms (41.59%), Space: 46.2 MB (68.68%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.java
@@ -22,31 +22,46 @@ class Node {
 */
 
 class Solution {
+    // public Node connect(Node root) {
+    //     if(root==null)return root;
+    //     traversal(root);
+    //     return root;
+    // }
+    // private void traversal(Node curr){
+    //     if(curr==null) return;
+    //     if(curr.left != null && curr.right !=null){
+    //         curr.left.next=curr.right;
+    //     }
+    //     if(curr.right != null){
+    //         curr.right.next=getNextChild(curr.next);
+    //     }else if(curr.left !=null ){
+    //         curr.left.next=getNextChild(curr.next);
+    //     }
+        
+    //     traversal(curr.right);
+    //     traversal(curr.left);
+    // }
+    // private Node getNextChild(Node curr){
+    //     while(curr !=null){
+    //         if(curr.left!=null)return curr.left;
+    //         if(curr.right!=null)return curr.right;
+    //         curr=curr.next;
+    //     }
+    //     return null;
+    // }
     public Node connect(Node root) {
         if(root==null)return root;
-        traversal(root);
+        Queue<Node> q=new LinkedList<>();
+        q.offer(root);
+        while(!q.isEmpty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node temp=q.poll();
+                if(i<n-1) temp.next=q.peek();
+                if(temp.left !=null)q.offer(temp.left);
+                if(temp.right!=null)q.offer(temp.right);
+            }
+        }
         return root;
-    }
-    private void traversal(Node curr){
-        if(curr==null) return;
-        if(curr.left != null && curr.right !=null){
-            curr.left.next=curr.right;
-        }
-        if(curr.right != null){
-            curr.right.next=getNextChild(curr.next);
-        }else if(curr.left !=null ){
-            curr.left.next=getNextChild(curr.next);
-        }
-        
-        traversal(curr.right);
-        traversal(curr.left);
-    }
-    private Node getNextChild(Node curr){
-        while(curr !=null){
-            if(curr.left!=null)return curr.left;
-            if(curr.right!=null)return curr.right;
-            curr=curr.next;
-        }
-        return null;
     }
 }
