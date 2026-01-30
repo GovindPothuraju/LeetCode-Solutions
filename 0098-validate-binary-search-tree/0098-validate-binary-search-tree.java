f207ef891163378f3c96c5e64c9a8ca9bf23844a
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-30 10:31:51 +0530

    Time: 1 ms (17.73%), Space: 45.1 MB (60.92%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -31,18 +31,54 @@ class Solution {
     //     res.add(curr.val);
     //     traversal(curr.right);
     // }
+
+    // 2. using recursion
     
+    // public boolean isValidBST(TreeNode root) {
+    //     boolean res=traversal(root,Long.MIN_VALUE,Long.MAX_VALUE);
+    //     return res;
+    // }
+    // private boolean traversal(TreeNode curr,long lo,long hi){
+    //     if(curr==null)return true;
+    //     if(curr.val<=lo || curr.val>=hi){
+    //        return false;
+    //     } 
+    //     boolean left=traversal(curr.left,lo,curr.val);
+    //     boolean right=traversal(curr.right,curr.val,hi);
+    //     return left && right;
+    // }
+
+    // 3 . using morris traversal
     public boolean isValidBST(TreeNode root) {
-        boolean res=traversal(root,Long.MIN_VALUE,Long.MAX_VALUE);
-        return res;
-    }
-    private boolean traversal(TreeNode curr,long lo,long hi){
-        if(curr==null)return true;
-        if(curr.val<=lo || curr.val>=hi){
-           return false;
-        } 
-        boolean left=traversal(curr.left,lo,curr.val);
-        boolean right=traversal(curr.right,curr.val,hi);
-        return left && right;
+        TreeNode curr=root;
+        long preValue = Long.MIN_VALUE;
+
+        while(curr != null){
+            if(curr.left==null){
+                if(preValue >= curr.val){
+                    return false;
+                }
+                preValue=curr.val;
+                curr=curr.right;
+            }else{
+                TreeNode ip=curr.left;
+                while(ip.right!=null && ip.right !=curr){
+                    ip=ip.right;
+                }
+
+                if(ip.right == null){
+                    ip.right=curr;
+                    curr=curr.left;
+                }else{
+                    ip.right=null;
+                    if(preValue >= curr.val){
+                        return false;
+                    }
+                    preValue=curr.val;
+                    curr=curr.right;
+                }
+            }
+        }
+        return true;
     }
 }
