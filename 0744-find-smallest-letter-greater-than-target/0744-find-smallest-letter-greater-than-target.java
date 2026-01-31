065f2630a95aa9623d2f50cd9c8a6b35920519ea
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-01-31 08:44:45 +0530

    Time: 0 ms (100%), Space: 45.9 MB (98.77%) - LeetHub

--- 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.java
@@ -3,16 +3,13 @@ class Solution {
         int l=0;
         int r=letters.length-1;
         while(l<=r){
-            int mid=(l+r)/2;
-            if(letters[mid]>target){
+            int mid=l+(r-l)/2;
+            if(letters[mid] > target){
                 r=mid-1;
             }else{
                 l=mid+1;
             }
         }
-        if(l>=letters.length){
-            l=0;
-        }
-        return letters[l];
+        return (l>letters.length-1)? letters[0] : letters[l];
     }
 }
