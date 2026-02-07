23ea0e2cf98cce26491d3a26d2f08c3315a43d5d
GovindPothuraju <govindpothuraju2006@gmail.com>
2026-02-07 09:03:33 +0530

    Time: 3 ms (37.87%), Space: 45.9 MB (32.21%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,22 +1,46 @@
 class Solution {
-    List<List<Integer>> res=new ArrayList<>();
+    // List<List<Integer>> res=new ArrayList<>();
+    // public List<List<Integer>> combinationSum(int[] candidates, int target) {
+    //     Arrays.sort(candidates);
+    //     backtrack(new ArrayList<>(),0,0,candidates,target);
+    //     return res;
+    // }
+    // private void backtrack(List<Integer> path,int start,int sum ,int[] arr,int tar){
+    //     if(sum==tar){
+    //         res.add(new ArrayList<>(path));
+    //         return;
+    //     }
+    //     for(int i=start;i<arr.length;i++){
+    //         if(sum+arr[i]>tar ) return;
+    //         path.add(arr[i]);
+    //         sum+=arr[i];
+    //         backtrack(path,i,sum,arr,tar);
+    //         path.remove(path.size()-1);
+    //         sum-=arr[i];
+    //     }
+    // }
+    List<List<Integer>> res = new ArrayList<>();
+    int n;
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         Arrays.sort(candidates);
-        backtrack(new ArrayList<>(),0,0,candidates,target);
+        n=candidates.length;
+        backtrack(new ArrayList<>(), 0, target, candidates);
         return res;
     }
-    private void backtrack(List<Integer> path,int start,int sum ,int[] arr,int tar){
-        if(sum==tar){
+
+    private void backtrack(List<Integer> path, int start, int target, int[] arr) {
+        if (target == 0) {
             res.add(new ArrayList<>(path));
             return;
         }
-        for(int i=start;i<arr.length;i++){
-            if(sum+arr[i]>tar ) return;
+
+        for (int i = start; i <n; i++) {
+            if (arr[i] > target) break; // 🔥 correct pruning
+
             path.add(arr[i]);
-            sum+=arr[i];
-            backtrack(path,i,sum,arr,tar);
-            path.remove(path.size()-1);
-            sum-=arr[i];
+            backtrack(path, i, target - arr[i], arr);
+            path.remove(path.size() - 1);
         }
     }
 }
+
