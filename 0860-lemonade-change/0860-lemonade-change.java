936bbe9b7120076aa1666d3a2a8dec85c13800c9
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-03-04 20:13:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,37 +1,65 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
+        // using hashmap
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // for(int i=0;i<bills.length;i++){
+        //     if(bills[i]==10){
+        //         if(hm.containsKey(5) && hm.get(5)>=1){
+        //             hm.put(5,hm.get(5)-1);
+        //             if(hm.get(5)==0){
+        //                 hm.remove(5);
+        //             }
+        //         }else{
+        //             return false;
+        //         }
+        //     }else if(bills[i]==20){
+        //         if(hm.containsKey(10) && hm.containsKey(5) && hm.get(10)>=1 && hm.get(5)>=1){
+        //             hm.put(10,hm.get(10)-1);
+        //             if(hm.get(10)==0){
+        //                 hm.remove(10);
+        //             }
+        //             hm.put(5,hm.get(5)-1);
+        //             if(hm.get(5)==0){
+        //                 hm.remove(5);
+        //             }
+        //         }else if(hm.containsKey(5) &&hm.get(5)>=3){
+        //             hm.put(5,hm.get(5)-3);
+        //             if(hm.get(5)==0){
+        //                 hm.remove(5);
+        //             }
+        //         }else{
+        //             return false;
+        //         }
+
+        //     }
+        //     hm.put(bills[i],hm.getOrDefault(bills[i],0)+1);
+        // }
+        // return true;
+
+        // greedy approach
+        int five=0;
+        int ten=0;
         for(int i=0;i<bills.length;i++){
             if(bills[i]==10){
-                if(hm.containsKey(5) && hm.get(5)>=1){
-                    hm.put(5,hm.get(5)-1);
-                    if(hm.get(5)==0){
-                        hm.remove(5);
-                    }
+                if(five>=1){
+                    five--;
                 }else{
                     return false;
                 }
-            }else if(bills[i]==20){
-                if(hm.containsKey(10) && hm.containsKey(5) && hm.get(10)>=1 && hm.get(5)>=1){
-                    hm.put(10,hm.get(10)-1);
-                    if(hm.get(10)==0){
-                        hm.remove(10);
-                    }
-                    hm.put(5,hm.get(5)-1);
-                    if(hm.get(5)==0){
-                        hm.remove(5);
-                    }
-                }else if(hm.containsKey(5) &&hm.get(5)>=3){
-                    hm.put(5,hm.get(5)-3);
-                    if(hm.get(5)==0){
-                        hm.remove(5);
-                    }
-                }else{
-                    return false;
-                }
-
             }
-            hm.put(bills[i],hm.getOrDefault(bills[i],0)+1);
+            else if(bills[i]==20){
+                if(ten>=1 && five>=1){
+                    ten--;
+                    five--;
+                }else if(five>=3){
+                    five-=3;
+                }else{
+                    return false;
+                }
+            }
+
+            if(bills[i]==5)five++;
+            else if(bills[i]==10)ten++;
         }
         return true;
     }
