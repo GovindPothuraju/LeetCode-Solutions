675433da4c3d2f446c7def6dc03cc67ff8eb9885
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-03-04 20:16:12 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -40,15 +40,14 @@ class Solution {
         int five=0;
         int ten=0;
         for(int i=0;i<bills.length;i++){
-            if(bills[i]==10){
-                if(five>=1){
-                    five--;
-                }else{
-                    return false;
-                }
-            }
-            else if(bills[i]==20){
-                if(ten>=1 && five>=1){
+            if(bills[i]==5){
+                five++;
+            }else if(bills[i]==10){
+                if(five==0)return false;
+                five--;
+                ten++;
+            }else if(bills[i]==20){
+                if(ten>0 && five>0){
                     ten--;
                     five--;
                 }else if(five>=3){
@@ -57,9 +56,6 @@ class Solution {
                     return false;
                 }
             }
-
-            if(bills[i]==5)five++;
-            else if(bills[i]==10)ten++;
         }
         return true;
     }
