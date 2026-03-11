0d8930586a6d660ad96237c5819b564fae391477
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-03-11 19:56:13 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
+++ 0435-non-overlapping-intervals/0435-non-overlapping-intervals.java
@@ -1,20 +1,37 @@
 class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
+        // 1. one method
+        // Arrays.sort(intervals,(a,b)->a[1]-b[1]);
+        // int n=intervals.length;
+        // ArrayList<int[]> arr=new ArrayList<>();
+        // for(int i=0;i<n;i++){
+        //     arr.add(intervals[i]);
+        // }
+        // System.out.println(Arrays.deepToString(arr.toArray()));
+        // int count=0;
+        // int i=1;
+        // while(i<arr.size()){
+        //     if(arr.get(i-1)[1] > arr.get(i)[0]){
+        //         arr.remove(arr.get(i));
+        //         count++;
+        //     }else{
+        //         i++;
+        //     }
+        // }
+        // return count;
+
+        // 2nd method
         Arrays.sort(intervals,(a,b)->a[1]-b[1]);
         int n=intervals.length;
-        ArrayList<int[]> arr=new ArrayList<>();
-        for(int i=0;i<n;i++){
-            arr.add(intervals[i]);
-        }
-        System.out.println(Arrays.deepToString(arr.toArray()));
+
+        int k=Integer.MIN_VALUE;
         int count=0;
-        int i=1;
-        while(i<arr.size()){
-            if(arr.get(i-1)[1] > arr.get(i)[0]){
-                arr.remove(arr.get(i));
+
+        for(int i=0;i<n;i++){
+            if(intervals[i][0] < k){
                 count++;
             }else{
-                i++;
+                k=intervals[i][1];
             }
         }
         return count;
