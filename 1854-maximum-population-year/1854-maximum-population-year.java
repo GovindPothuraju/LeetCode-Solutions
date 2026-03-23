c9ae5b31db8608e3bbc070f46a4c28a026c1653d
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-03-23 14:49:50 +0530

    Time: 19 ms (5.81%), Space: 44.6 MB (5.92%) - LeetHub

--- 1854-maximum-population-year/1854-maximum-population-year.java
+++ 1854-maximum-population-year/1854-maximum-population-year.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int maximumPopulation(int[][] logs) {
+    // 1)using difference array
+    /*public int maximumPopulation(int[][] logs) {
         int[] years=new int[2051];
         for(int i=0;i<logs.length;i++){
             int birth=logs[i][0];
@@ -19,5 +20,41 @@ class Solution {
         }
 
         return res;
+    }*/
+
+    // 2) using linesweep algorithm
+    public int maximumPopulation(int[][] logs) {
+        List<int[]> events = new ArrayList<>();
+
+        // Step 1: Create events
+        for (int[] log : logs) {
+            events.add(new int[]{log[0], 1});  // birth
+            events.add(new int[]{log[1], -1}); // death
+        }
+
+        // Step 2: Sort events
+        Collections.sort(events, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1]; // +1 before -1
+            return a[0] - b[0];
+        });
+
+        for (int[] e : events) {
+            System.out.println(Arrays.toString(e));
+        }
+        int curr = 0;
+        int maxPop = 0;
+        int year = 0;
+
+        // Step 3: Sweep
+        for (int[] e : events) {
+            curr += e[1];
+
+            if (curr > maxPop) {
+                maxPop = curr;
+                year = e[0];
+            }
+        }
+
+        return year;
     }
 }
