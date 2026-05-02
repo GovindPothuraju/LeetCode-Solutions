4231afeb169433be320af6426fc06fba0ec33a49
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-05-02 10:57:48 +0530

    Time: 82 ms (5.74%), Space: 46.9 MB (99.74%) - LeetHub

---
+++ 0130-surrounded-regions/0130-surrounded-regions.java
@@ -0,0 +1,70 @@
+class Solution {
+    public void solve(char[][] board) {
+        int n=board.length;
+        int m=board[0].length;
+
+        // ckeck first row 
+        for(int j=0;j<m;j++){
+            if(board[0][j]=='O'){
+                backtrack(0,j,n,m,board);
+            }
+        }
+        //check first col
+        for(int i=0;i<n;i++){
+            if(board[i][0]=='O'){
+                backtrack(i,0,n,m,board);
+            }
+        }
+        // check last row
+        for(int j=0;j<m;j++){
+            if(board[n-1][j]=='O'){
+                backtrack(n-1,j,n,m,board);
+            }
+        }
+        // check last col
+        for(int i=0;i<n;i++){
+            if(board[i][m-1]=='O'){
+                backtrack(i,m-1,n,m,board);
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(board[i][j]+" ");
+            }
+            System.out.println();
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='O'){
+                    board[i][j]='X';
+                }
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j]=='T'){
+                    board[i][j]='O';
+                }
+            }
+        }
+    }
+    private void backtrack(int row,int col,int n,int m,char[][] board){
+        board[row][col]='T';
+        // top
+        if(row>0 && board[row-1][col]=='O'){
+            backtrack(row-1,col,n,m,board);
+        }
+        // left
+        if(col>0 && board[row][col-1]=='O'){
+            backtrack(row,col-1,n,m,board);
+        }
+        // bottom
+        if(row<n-1 && board[row+1][col]=='O'){
+            backtrack(row+1,col,n,m,board);
+        }
+        // right
+        if(col<m-1 && board[row][col+1]=='O'){
+            backtrack(row,col+1,n,m,board);
+        }
+    }
+}
