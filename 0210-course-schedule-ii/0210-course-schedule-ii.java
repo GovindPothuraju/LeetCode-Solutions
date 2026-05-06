0bbeaee3ed7ff202bf846d893eb75f6a1560243b
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-05-06 20:41:33 +0530

    Time: 3 ms (99.53%), Space: 46.9 MB (56.68%) - LeetHub

--- 0210-course-schedule-ii/0210-course-schedule-ii.java
+++ 0210-course-schedule-ii/0210-course-schedule-ii.java
@@ -1,4 +1,5 @@
 class Solution {
+    /* using khan's algorithm
     public int[] findOrder(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
@@ -7,6 +8,7 @@ class Solution {
         for(int[] edges : prerequisites){
             int u=edges[0];
             int v=edges[1];
+            //If I take this edge direction, will indegree=0 mean ‘ready to take’?
             adj.get(v).add(u);
         }
         // find all the indegrees
@@ -37,5 +39,61 @@ class Solution {
         }
         if(k != numCourses) return new int[0];
         return res;
+    }*/
+    
+    // using topological sort by using dfs (bottom-up approach) + topological sort
+    int[] res;
+    int k=0;
+    public int[] findOrder(int numCourses, int[][] prerequisites) {
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] edges : prerequisites){
+            int u=edges[0];
+            int v=edges[1];
+            adj.get(v).add(u);
+        }
+        boolean[] visited=new boolean[numCourses];
+        boolean[] path=new boolean[numCourses];
+        res=new int[numCourses];
+        k=0;
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(backtrack(i,visited,adj,path)){
+                    return new int[0];
+                }
+            }
+        }
+        reverse(res);
+        return res;
     }
+    private boolean backtrack(int node,boolean[] visited,List<List<Integer>> adj,boolean[] path){
+        visited[node]=true;
+        path[node]=true;
+        for(int nei:adj.get(node)){
+            if(!visited[nei]){
+                if(backtrack(nei,visited,adj,path)){
+                    return true;
+                }
+            }else if(path[nei]){
+                return true;
+            }
+        }
+        path[node]=false;
+        res[k++]=node;
+        return false;
+    }
+    private void reverse(int[] res){
+        int l=0;
+        int r=res.length-1;
+        while(l<r){
+            int temp=res[l];
+            res[l]=res[r];
+            res[r]=temp;
+            l++;
+            r--;
+        }
+    }
+
 }
