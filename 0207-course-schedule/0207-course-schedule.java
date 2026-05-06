7ec407804f61a4158ebea077136abbc60b96db20
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-05-06 20:12:38 +0530

    Time: 4 ms (93.21%), Space: 47.3 MB (20.38%) - LeetHub

--- 0207-course-schedule/0207-course-schedule.java
+++ 0207-course-schedule/0207-course-schedule.java
@@ -1,4 +1,5 @@
 class Solution {
+    /* using BFS OR khan's algorithm
     public boolean canFinish(int numCourses, int[][] prerequisites) {
         List<List<Integer>> adj=new ArrayList<>();
         for(int i=0;i<numCourses;i++){
@@ -7,7 +8,7 @@ class Solution {
         for(int[] edges : prerequisites){
             int u=edges[0];
             int v=edges[1];
-            adj.get(u).add(v);
+            adj.get(v).add(u);
         }
         // find all the indegrees
         int[] indegree = new int[numCourses];
@@ -36,5 +37,46 @@ class Solution {
         // the k value represents the no of nodes are visited linearly u must visited before v 
         // if k==n that mean all nodes are visited topologically and no cyclye exists
         return k==numCourses;
+    }*/
+
+    // using dfs
+    public boolean canFinish(int numCourses, int[][] prerequisites){
+
+        List<List<Integer>> adj=new ArrayList<>();
+        for(int i=0;i<numCourses;i++){
+            adj.add(new ArrayList<>());
+        }
+        for(int[] temp : prerequisites){
+            int u=temp[0];
+            int v=temp[1];
+            adj.get(v).add(u);
+        }
+        
+        boolean[] visited=new boolean[numCourses];
+        boolean[] path=new boolean[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(!visited[i]){
+                if(backtrack(i,adj,visited,path)){
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+    private boolean backtrack(int node,List<List<Integer>> adj,boolean[] visited,boolean[] path){
+        visited[node]=true;
+        path[node]=true;
+        for(int nei:adj.get(node)){
+            if(!visited[nei]){
+                if(backtrack(nei,adj,visited,path)){
+                    return true;
+                }
+            }else if(path[nei]){
+                return true;
+            }
+        }
+        path[node]=false;
+        return false;
     }
 }
