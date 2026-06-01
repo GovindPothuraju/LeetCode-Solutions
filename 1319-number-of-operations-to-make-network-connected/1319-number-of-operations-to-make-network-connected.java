effea38c4f8946c4a297ef8b068c7325ea1c41ee
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-01 13:25:45 +0530

    Time: 14 ms (30.96%), Space: 68.4 MB (33.68%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.java
@@ -1,4 +1,6 @@
-class Solution {
+// using df
+
+/*class Solution {
     public int makeConnected(int n, int[][] connections) {
         if(connections.length < n-1){
             return -1;
@@ -30,5 +32,49 @@ class Solution {
                 backtrack(nei,adj,visited);
             }
         }
+    }
+}*/
+
+class Solution {
+    int[] parent;
+    int[] size;
+    public int find(int node){
+        if(node == parent[node])return node;
+        return parent[node]=find(parent[node]);
+    }
+    public boolean unionFind(int u,int v){
+        int pu=find(u);
+        int pv=find(v);
+        if(pu==pv)return true;
+        if(size[pu] < size[pv]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+        return false;
+    }
+    public int makeConnected(int n, int[][] connections) {
+        if(connections.length < n-1){
+            return -1;
+        }
+        parent =new int[n];
+        size=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+        int count=0;
+        for(int[] temp : connections){
+            int u=temp[0];
+            int v=temp[1];
+            unionFind(u,v);
+        }
+        System.out.println(Arrays.toString(parent));
+        for(int i=0;i<n;i++){
+            if(parent[i] == i)count++;
+        }
+        return count-1;
     }
 }
