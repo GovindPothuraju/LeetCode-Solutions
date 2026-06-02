aac3b316760aaa42136985be13ac4f73826afb92
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-02 13:12:26 +0530

    Time: 27 ms (42.26%), Space: 47.1 MB (32.51%) - LeetHub

--- 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.java
@@ -1,4 +1,61 @@
 class Solution {
+    int[] parent;
+    int[] size;
+    private int find(int node){
+        if(parent[node]==node){
+            return node;
+        }
+        return parent[node]=find(parent[node]);
+    }
+    private void unionf(int u,int v){
+        int pu = find(u);
+        int pv = find(v);
+        if(pu == pv)return ;
+        if(size[pu] < size[pv]){
+            parent[pu]=pv;
+            size[pv]+=size[pu];
+        }else{
+            parent[pv]=pu;
+            size[pu]+=size[pv];
+        }
+    }
+    public int removeStones(int[][] stones) {
+        int n=stones.length;
+
+        parent = new int[n];
+        size=new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+            size[i]=1;
+        }
+
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int row=stones[i][0];
+                int col=stones[i][1];
+
+                int otherRow=stones[j][0];
+                int otherCol=stones[j][1];
+
+                if(row==otherRow || col==otherCol){
+                    unionf(i,j);
+                }
+            }
+        }
+
+        int components=0;
+        for(int i=0;i<n;i++){
+            if(parent[i]==i){
+                components++;
+            }
+        }
+        return n-components;
+    }
+}
+
+
+
+/*class Solution {
     public int removeStones(int[][] stones) {
         int n=stones.length;
         int components = 0;
@@ -23,4 +80,4 @@ class Solution {
             }
         }
     }
-}
+}*/
