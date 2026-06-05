2ee251c637875c18cf4bddaca915e2d68bacd0c2
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-05 19:28:18 +0530

    Time: 3 ms (96.1%), Space: 46.1 MB (91.59%) - LeetHub

--- 0778-swim-in-rising-water/0778-swim-in-rising-water.java
+++ 0778-swim-in-rising-water/0778-swim-in-rising-water.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int swimInWater(int[][] grid) {
+    // using pq+ greedy =>dijkstar's
+    /*public int swimInWater(int[][] grid) {
         PriorityQueue<int[]> pq=new PriorityQueue<>(
             (a,b)->a[2]-b[2]
         );
@@ -40,5 +41,57 @@ class Solution {
             }
         }
         return max;
+    }*/
+
+    public int swimInWater(int[][] grid){
+        int n = grid.length;
+
+        int l=0;
+        int r=n*n;
+        int res=0;
+        while(l<=r){
+            int mid = l +(r-l)/2;
+            if(isPossible(mid,grid,n)){
+                res=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return res;
+    }
+    private boolean isPossible(int k,int[][]grid,int n){
+        boolean[][] visited = new boolean[n][n];
+        return backtrack(0,0,grid,k,visited,n);
+    }
+    private boolean backtrack(int row,int col,int[][] grid,int k,boolean[][] visited,int n){
+        if(grid[row][col] > k || visited[row][col]) return false;
+        if(row==n-1 && col ==n-1) return true;
+        visited[row][col]=true;
+        // top 
+        if(row >0 && !visited[row-1][col]){
+            if(backtrack(row-1,col,grid,k,visited,n)){
+                return true;
+            }
+        }
+        //left
+        if(col >0 && !visited[row][col-1]){
+            if(backtrack(row,col-1,grid,k,visited,n)){
+                return true;
+            }
+        }
+        //bottom
+        if(row < n-1 && !visited[row+1][col]){
+            if(backtrack(row+1,col,grid,k,visited,n)){
+                return true;
+            }
+        }
+        //right
+        if(col < n-1 && !visited[row][col+1]){
+            if(backtrack(row,col+1,grid,k,visited,n)){
+                return true;
+            }
+        }
+        return false;
     }
 }
