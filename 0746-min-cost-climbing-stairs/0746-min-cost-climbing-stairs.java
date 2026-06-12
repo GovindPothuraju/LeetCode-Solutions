c874d5265791fc7aad0ba284c2d42d69d4415061
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-12 08:18:48 +0530

    Time: 1 ms (38.74%), Space: 44.7 MB (87.54%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,5 +1,6 @@
 class Solution {
-    int top ;
+    // top down
+    /*int top ;
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
         top = n;
@@ -11,6 +12,16 @@ class Solution {
         if(n<=1) return cost[n];
         if(dp[n] != -1)return dp[n];
         dp[n] = ((n==top)?0:cost[n])+ Math.min(climb(n-1,dp,cost) , climb(n-2,dp,cost));
+        return dp[n];
+    }*/
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        dp[0]=0;
+        dp[1]=0;
+        for(int i=2;i<=n;i++){
+            dp[i] = Math.min(dp[i-1]+cost[i-1] , dp[i-2]+cost[i-2]);
+        }
         return dp[n];
     }
 }
