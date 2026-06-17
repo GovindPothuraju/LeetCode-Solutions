371e3bd1a7e424062bb432d9f16cc33c9f2faa4a
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-17 18:16:37 +0530

    Time: 1 ms (82.72%), Space: 42.7 MB (96.55%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.java
+++ 0091-decode-ways/0091-decode-ways.java
@@ -31,14 +31,16 @@
 }*/
 
 // using memoization 
-class Solution {
+/*class Solution {
     public int numDecodings(String s) {
         int n = s.length();
         if(s.charAt(0) == '0') return 0;
         if(n==1) return 1;
         int[] dp = new int[n];
         Arrays.fill(dp,-1);
-        return backtrack(0,s,dp);
+        int res = backtrack(0,s,dp);
+        System.out.println(Arrays.toString(dp));
+        return res;
     }
     private int backtrack(int st,String s,int[] dp){
         // base case
@@ -63,5 +65,29 @@ class Solution {
         }
         dp[st] = ways;
         return dp[st];
+    }
+}*/
+
+// using tabulation
+class Solution {
+    public int numDecodings(String s) {
+        int n = s.length();
+        if(s.charAt(0) == '0') return 0;
+
+        int[] dp = new int[n+1];
+
+        dp[0] = 1;
+        dp[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            if(s.charAt(i-1) != '0'){
+                dp[i] += dp[i-1];
+            }
+            int num = (s.charAt(i-2)-'0')*10 + (s.charAt(i-1)-'0');
+            if(num>=10 && num<=26){
+                dp[i] += dp[i-2];
+            }
+        }
+        return dp[n];
     }
 }
