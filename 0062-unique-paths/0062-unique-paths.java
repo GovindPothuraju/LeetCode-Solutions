b87e57ac405961b3110492658c9b78a837aa6a18
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-06-18 07:39:56 +0530

    Time: 0 ms (100%), Space: 41.8 MB (91.2%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
     // i used backtracking but the time compleity is crossed that's why i used a simple formula
     // int m;
     // int n;
@@ -23,17 +23,37 @@ class Solution {
     //         backtrack(x,y+1);
     //     }
     // }
-     public int uniquePaths(int m, int n) {
-        int total = m + n - 2;
-        int r = Math.min(m - 1, n - 1);
+    //  public int uniquePaths(int m, int n) {
+    //     int total = m + n - 2;
+    //     int r = Math.min(m - 1, n - 1);
 
-        long result = 1;
+    //     long result = 1;
 
-        for (int i = 1; i <= r; i++) {
-            result = result * (total - r + i) / i;
+    //     for (int i = 1; i <= r; i++) {
+    //         result = result * (total - r + i) / i;
+    //     }
+
+    //     return (int) result;
+    // }
+
+    
+
+}*/
+
+class Solution {
+    public int uniquePaths(int n, int m) {
+        int[][] dp = new int[n][m];
+        dp[0][0] = 1;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(i>0){
+                    dp[i][j]+=dp[i-1][j];
+                }
+                if(j>0){
+                    dp[i][j]+=dp[i][j-1];
+                }
+            }
         }
-
-        return (int) result;
+        return dp[n-1][m-1];
     }
-
 }
