2b2f7e7e03bfa145179d83d0dacacfa449436988
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-07-01 20:30:45 +0530

    Time: 31 ms (29.33%), Space: 46.4 MB (60.87%) - LeetHub

--- 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.java
@@ -47,7 +47,9 @@
     }
 }*/
 
-class Solution {
+// using memoization
+
+/*class Solution {
     int n,m;
     public int cherryPickup(int[][] grid) {
         // Your code goes here
@@ -59,6 +61,7 @@ class Solution {
                 Arrays.fill(dp[i][j], -1);
             }
         }
+        
         return backtrack(0,0,m-1,grid,dp);
         
     }
@@ -97,5 +100,59 @@ class Solution {
             }
         }
         return dp[row][c1][c2] = best;
+    }
+}*/
+
+// using tabulation
+class Solution {
+    int n,m;
+    public int cherryPickup(int[][] grid) {
+        // Your code goes here
+        this.n = grid.length;
+        this.m = grid[0].length;
+        int[][][] dp = new int[n][m][m];
+        
+        // base case fill all border values
+        for(int c1=0;c1<m;c1++){
+            for(int c2=0;c2<m;c2++){
+                if(c1==c2){
+                    dp[n-1][c1][c2] = grid[n-1][c1];
+                }else{
+                    dp[n-1][c1][c2] = grid[n-1][c1]+grid[n-1][c2];
+                }
+            }
+        }
+
+        // fill remaing rows from bottom to top
+        for(int row=n-2;row>=0;row--){
+            for(int c1=0;c1<m;c1++){
+                for(int c2=0;c2<m;c2++){
+                    int current;
+                    if(c1==c2){
+                        current = grid[row][c1];
+                    }else{
+                        current = grid[row][c1]+grid[row][c2];
+                    }
+                    int best = -(int)1e9;
+                    // check for all 9 possibilites
+                    for(int val1=-1 ; val1<=1; val1++){
+                        for(int val2=-1; val2<=1 ; val2++){
+                            int newCol1 = c1+val1;
+                            int newCol2 = c2+val2;
+
+                            int newRow = row+1;
+                            //maximize best amonog 9
+                            if(newCol1 >=0  && newCol1<m && newCol2>=0  && newCol2<m ){
+                                best = Math.max(best, current + dp[newRow][newCol1][newCol2]);
+                            }
+                        }
+                    }
+                    dp[row][c1][c2] = best;
+                }
+            }
+        }
+        
+        return dp[0][0][m-1];
+        
     }
 }
