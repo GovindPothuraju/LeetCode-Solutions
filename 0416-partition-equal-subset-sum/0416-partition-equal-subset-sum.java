bf2f7810947f6d374b600465cba82ab972456d72
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-07-12 00:09:42 +0530

    Time: 88 ms (25.35%), Space: 72.6 MB (27.58%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -32,7 +32,6 @@
 
 // uisng dp
 class Solution {
-    Boolean[][] dp ;
     public boolean canPartition(int[] nums) {
         int n = nums.length;
         int sum = 0;
@@ -41,25 +40,25 @@ class Solution {
         if(sum%2!=0) return false;
 
         int target = sum/2;
-        dp = new Boolean[n][target+1];
-        for(int i=0;i<n;i++){
-            if(backtrack(i,target-nums[i],nums)){
-                return true;
-            }
+
+        int[][] dp = new int[n][target+1];
+        for(int[] temp : dp){
+            Arrays.fill(temp,-1);
         }
-        return false;
+        return backtrack(n-1,target,nums,dp);
     }
-    private boolean backtrack(int st,int sum,int[] nums){
-        if(sum == 0)return true;
-        if(sum<0)return false;
+    private boolean backtrack(int st,int target,int[] nums,int[][] dp){
+        if(target == 0)return true;
+        if(st == 0)return nums[0]==target;
 
-        if(dp[st][sum] != null) return dp[st][sum];
+        if(dp[st][target] != -1) return dp[st][target]==0?false:true;
 
-        for(int i=st+1;i<nums.length;i++){
-            if(backtrack(i,sum-nums[i],nums)){
-                return dp[st][sum] = true;
-            }
+        boolean notTaken = backtrack(st-1,target,nums,dp);
+        boolean taken = false;
+        if(target >= nums[st]){
+            taken = backtrack(st-1,target-nums[st],nums,dp);
         }
-        return dp[st][sum] = false;
+        dp[st][target] = taken || notTaken ? 1 : 0;
+         return dp[st][target]==0?false:true;
     }
 }
