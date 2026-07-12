e102ea0f8b9421408549213cc49c85b6cd4c178f
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-07-12 13:00:42 +0530

    Time: 76 ms (5.22%), Space: 78.1 MB (10.88%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,21 +1,62 @@
+/*class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        // PriorityQueue<Integer> pq = new PriorityQueue<>();
+        // for(int num:arr){
+        //     pq.add(num);
+        // }
+        // HashMap<Integer,Integer> hm=new HashMap<>();
+        // int i=1;
+        // while(pq.size()>0){
+        //     if(!hm.containsKey(pq.peek())){
+        //         hm.put(pq.poll(),i++);
+        //     }else{
+        //         pq.poll();
+        //     }
+            
+        // }
+        // for(i=0;i<arr.length;i++){
+        //     arr[i]=hm.get(arr[i]);
+        // }
+        // return arr;
+        // int[] sorted = arr.clone();
+        // Arrays.sort(sorted);
+
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // int rank = 1;
+
+        // for (int num : sorted) {
+        //     if (!map.containsKey(num)) {
+        //         map.put(num, rank++);
+        //     }
+        // }
+
+        // for (int i = 0; i < arr.length; i++) {
+        //     arr[i] = map.get(arr[i]);
+        // }
+
+        // return arr;
+    }
+}*/
+
+
+
+
 class Solution {
     public int[] arrayRankTransform(int[] arr) {
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         for(int num:arr){
             pq.add(num);
         }
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        int i=1;
-        while(pq.size()>0){
-            if(!hm.containsKey(pq.peek())){
-                hm.put(pq.poll(),i++);
-            }else{
-                pq.poll();
+        int n = 1;
+        HashMap<Integer,Integer> hm = new HashMap<>();
+        while(!pq.isEmpty()){
+            int val = pq.poll();
+            if(!hm.containsKey(val)){
+                hm.put(val,n++);
             }
-            
         }
-        for(i=0;i<arr.length;i++){
-            arr[i]=hm.get(arr[i]);
+        for(int i=0;i<arr.length;i++){
+            arr[i] = hm.get(arr[i]);
         }
         return arr;
     }
