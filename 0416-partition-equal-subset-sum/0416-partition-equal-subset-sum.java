f0476592d6663ee2344ae4cedd8319c51c3a32cc
Govindha Pothuraju <govindpothuraju2006@gmail.com>
2026-07-24 14:33:27 +0530

    Time: 49 ms (51.03%), Space: 47.8 MB (43.68%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.java
@@ -1,3 +1,38 @@
+// class Solution {
+//     public boolean canPartition(int[] nums) {
+//         int n = nums.length;
+//         int sum = 0;
+//         for (int num : nums) {
+//             sum += num;
+//         }
+//         if (sum % 2 != 0)
+//             return false;
+//         int target = sum/2;
+//         int[][] dp = new int[n][target + 1];
+//         for (int[] temp : dp) {
+//             Arrays.fill(temp, -1);
+//         }
+//         return backtrack(n - 1, nums, target, dp);
+//     }
+
+//     private boolean backtrack(int st, int[] arr, int target, int[][] dp) {
+//         if (target == 0)
+//             return false;
+//         if (st == 0)
+//             return arr[st] == target;
+
+//         if (dp[st][target] != -1)
+//             return dp[st][target] == 1 ? true : false;
+//         boolean notTake = backtrack(st - 1, arr, target, dp);
+//         boolean take = false;
+//         if (target >= arr[st]) {
+//             take = backtrack(st - 1, arr, target - arr[st], dp);
+//         }
+//         dp[st][target] = take || notTake ? 1 : 0;
+//         return take || notTake;
+//     }
+// }
+
 class Solution {
     public boolean canPartition(int[] nums) {
         int n = nums.length;
@@ -8,27 +43,21 @@ class Solution {
         if (sum % 2 != 0)
             return false;
         int target = sum/2;
-        int[][] dp = new int[n][target + 1];
-        for (int[] temp : dp) {
-            Arrays.fill(temp, -1);
+        boolean[][] dp = new boolean[n][target + 1];
+        for(int i=0;i<n;i++){
+            dp[i][0]=true;
         }
-        return backtrack(n - 1, nums, target, dp);
-    }
-
-    private boolean backtrack(int st, int[] arr, int target, int[][] dp) {
-        if (target == 0)
-            return false;
-        if (st == 0)
-            return arr[st] == target;
-
-        if (dp[st][target] != -1)
-            return dp[st][target] == 1 ? true : false;
-        boolean notTake = backtrack(st - 1, arr, target, dp);
-        boolean take = false;
-        if (target >= arr[st]) {
-            take = backtrack(st - 1, arr, target - arr[st], dp);
+        if(nums[0]<=target)dp[0][nums[0]]=true;
+        for(int st=1;st<n;st++){
+            for(int tar=1;tar<=target;tar++){
+                boolean notTake = dp[st-1][tar];
+                boolean take = false;
+                if (tar >= nums[st]) {
+                    take = dp[st-1][tar-nums[st]];
+                }
+                dp[st][tar] = take || notTake;
+            }
         }
-        dp[st][target] = take || notTake ? 1 : 0;
-        return take || notTake;
+        return dp[n-1][target];
     }
 }
